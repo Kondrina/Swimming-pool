f2d1dabf3c613f81b56b34b4f00cbb80a19313ac
Helga Kondrina <kondinaolya@gmail.com>
2024-11-12 11:23:14 +0300

    Enter e-mail

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,7 @@ public class Main {
                 System.out.println("Можно купаться");
                 break;
             }
+            System.out.println("Введите e-mail: ");
         }
 
     }
