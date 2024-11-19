65871b179af07129630d19da70f1f3e599608ca2
Helga Kondrina <kondinaolya@gmail.com>
2024-11-19 12:56:54 +0300

    dopolnitelnoe izmenenie

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main {
             }
         }
         System.out.println("Звонить по номеру: 8-800" );
+        System.out.println("Попросили сделать еще одно изменение");
 
     }
 }
